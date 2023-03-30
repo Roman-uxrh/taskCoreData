8cbbbb38f12897f00683c39ecb64d74e1753b4ee
RRDubina <dubinaroman.dubina@gmail.com>
2023-03-30 13:45:02 +0500

    Add change - set up the project
    Delete mainStoryBoard, create new folders

---
+++ taskCoreData/Sourse/App/SceneDelegate.swift
@@ -0,0 +1,34 @@
+//
+//  SceneDelegate.swift
+//  taskCoreData
+//
+//  Created by admin on 29.03.2023.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        let viewController = ViewController()
+        let navigationController = UINavigationController(rootViewController: viewController)
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+    }
+    
+    func sceneDidEnterBackground(_ scene: UIScene) {
+        // Called as the scene transitions from the foreground to the background.
+        // Use this method to save data, release shared resources, and store enough scene-specific state information
+        // to restore the scene back to its current state.
+
+        // Save changes in the application's managed object context when the application transitions to the background.
+        (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
+    }
+
+
+}
+
